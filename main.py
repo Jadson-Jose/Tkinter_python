62a53dd9c35971d67041075aee3f7aa2d930c288
Jadson José da Silva <69395306+Jadson-Jose@users.noreply.github.com>
2023-03-14 18:03:33 -0300

    sinc

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("App Teste")
+root.geometry("700x400+200+200")
+root.maxsize(width=900, height=550)
+root.minsize(width=500, height=300)
+root.resizable(width=False, height=False)
+
+
+
+
+
+
+
+
+root.mainloop()
