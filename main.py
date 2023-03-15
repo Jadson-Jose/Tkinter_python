6ac95aa0b9212a9e5e9382da3daadff16ef64a94
Jadson José da Silva <69395306+Jadson-Jose@users.noreply.github.com>
2023-03-15 14:56:14 -0300

    sinc

--- main.py
+++ main.py
@@ -3,16 +3,35 @@ from tkinter import ttk
 
 root = Tk()
 root.title("App Teste")
-root.geometry("700x400+200+200")
-root.maxsize(width=900, height=550)
-root.minsize(width=500, height=300)
-root.resizable(width=False, height=False)
+
+# dimensões da janela
+largura = 500
+altura = 300
+
+# resolução do sistema
+largura_screen = root.winfo_screenwidth()
+altura_screen = root.winfo_screenheight()
+
+# posição da janela
+posx = largura_screen/2 - largura/2
+posy = altura_screen/2 - altura/2
+
+# definir a geometria
+root.geometry("%dx%d+%d+%d" % (largura, altura, posx, posy))
+
+label_1 = Label(root, 
+                text="Este é o label um\n Frase dois",
+                font="Arial 20",
+                borderwidth=5,
+                relief="raised").pack()
 
 
 
 
+btn = Button(root, text="Executar")
 
 
-
+# pack
+btn.pack()
 
 root.mainloop()
